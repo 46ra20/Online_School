a61bda6e960bf0409ec938979cd769e93d7c5346
Md Rakib Bhuiyan <r2046.md@gmail.com>
2024-07-08 22:57:33 +0600

    many to one relation

--- course/models.py
+++ course/models.py
@@ -20,7 +20,7 @@ class CourseModel(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=5,decimal_places=2)
 
-    department = models.ManyToOneRel(CourseCategory,on_delete=models.CASCADE)
+    department = models.ForeignKey(CourseCategory,on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
